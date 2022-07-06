80252a3f78eeacaacd3fbb6fe7c48288e2a458d2
Luyen Dao Dinh <91018384+henrydao15@users.noreply.github.com>
2022-07-06 17:30:51 +0700

    css

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
     var country_list = [];
     if (country != null) {
         country_list = country.map((element) => (
-            <li>
+            <li className='text-nowrap'>
                 <Link to={"/categories/" + element.country}>
                     <img src={element.image} alt={"img"} />
                     {element.country}
